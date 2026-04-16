bbeeea35748b484c8ae837c22f49703b61df4eba
kuldeep rajpurohit <35566422+ksrajpurohit@users.noreply.github.com>
2026-04-16 22:36:17 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # T O(n + m) || S O(n + m)
+        if len(s) != len(t): return False
+
+        frequency_map = {}
+
+        for ch in s:
+            frequency_map[ch] = frequency_map.get(ch, 0) + 1
+
+        for ch in t:
+            frequency_map[ch] = frequency_map.get(ch, 0) - 1
+
+        for k, v in frequency_map.items():
+            if v != 0:
+                return False
+
+        return True
